c39cd094690694ef2b45d34798b5debe469c9c99
Leandro Wilker Paulino Sousa <83305893+megall127@users.noreply.github.com>
2022-01-14 16:24:43 -0300

    SplashScreen

--- HoogliApp/App.js
+++ HoogliApp/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import GlobalState from './src/globalState/GlobalState';
 import Routes from './src/navigation/routes';
+import SplashScreen from 'react-native-splash-screen';
+
 
 
 const App =()=> {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  },[])
 
   return (
     <GlobalState>
